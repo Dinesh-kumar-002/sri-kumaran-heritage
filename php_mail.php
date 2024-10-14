3ccbbfb7401b45f9c1acc5bedc821483ed372c53
Dinesh-kumar-002 <dkdon812002@gmail.com>
2024-10-14 08:31:36 +0530

    added icons

---
+++ php_mail.php
@@ -0,0 +1,28 @@
+<?php
+
+$name= $_POST['name'];
+$phone= $_POST['phone'];
+$email= $_POST['email'];
+$message= $_POST['message'];
+   require 'vendor/autoload.php';
+   use PHPMailer\PHPMailer\PHPMailer;
+   $mail = new PHPMailer;
+   $mail->isSMTP();
+   $mail->SMTPDebug = false;
+   $mail->Host = 'smtp.gmail.com';
+   $mail->Port = 587;
+   $mail->SMTPAuth = true;
+   $mail->Username = 'quotationmailservice@gmail.com';
+   $mail->Password = 'otgu cxjv gzcf dsix';
+   $mail->setFrom('quotationmailservice@gmail.com', "Client");
+   $mail->addReplyTo($email, $name);
+   $mail->addAddress('dkdon812002@gmail.com', 'YOU GOT A QUOTATION');
+   $mail->Subject = 'Quotation mail from client';
+   $mail->Body = "Name: $name \n Phone: $phone \n Email Id: $email \n Message: $message";
+       if (!$mail->send()) {
+        //   echo 'Mailer Error: ' . $mail->ErrorInfo;
+        echo '<div class="alert alert-success">Something Want Wrong !!!!</div>';
+       } else {
+       echo 'success';
+    }
+?>
